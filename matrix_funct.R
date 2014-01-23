70bbe249710184202aff96b03fe790762562421e
Alessandra Breschi <alessandra.breschi@crg.es>
2014-01-23 12:07:55 +0100

    correct how it deals with functions applied to the whole matrix

--- matrix_funct.R
+++ matrix_funct.R
@@ -1,6 +1,4 @@
-
-# This script is useful for:
-# normalizing samples by row scaling
+#!/usr/bin/env Rscript
 
 
 ##------------
@@ -17,7 +15,6 @@ cat("DONE\n\n")
 
 
 options(stringsAsFactors=F)
-pseudocount = 1e-05
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") 
 
 
@@ -41,15 +38,34 @@ opt$replace_na = FALSE
 ##################
 
 option_list <- list(
-make_option(c("-i", "--input_matrix"), help="the matrix you want to analyze"),
-make_option(c("-l", "--log"), action="store_true", default=FALSE, help="apply the log [default=FALSE]"),
-make_option(c("-k", "--replace_na"), action="store_true", default=FALSE, help="use this if you want NAs to be replaces by 0 [default=%default]"),
-make_option(c("-p", "--pseudocount"), type="double", help="specify a pseudocount for the log [default=%default]", default=pseudocount),
-make_option(c("-m", "--metadata"), help="tsv file with the metadata"),
-make_option(c("-s", "--mean_by"), help="choose one or multiple attributes you want to average by"),
-make_option(c("-o", "--output_suffix"), help="additional output suffix [default=%default]", default='out'),
-make_option(c("-f", "--func"), help="choose the function <mean>, <sd>, <sum>, <median>, <entropy> [default=%default]", default='mean'),
-make_option(c("-n", "--not_na"), help="fraction of not NA values in the vector for the mean [default=%default]", default=1, type='double')
+
+make_option(c("-i", "--input_matrix"), 
+	help="the matrix you want to analyze"),
+
+make_option(c("-l", "--log"), action="store_true", default=FALSE, 
+	help="apply the log10, before applying the function [default=%default]"),
+
+make_option(c("-k", "--replace_na"), action="store_true", default=FALSE, 
+	help="use this if you want NAs to be replaced by 0 [default=%default]"),
+
+make_option(c("-p", "--pseudocount"), type="double", default=1e-04,
+	help="specify a pseudocount for the log [default=%default]"),
+
+make_option(c("-m", "--metadata"), 
+	help="tsv file with the metadata"),
+
+make_option(c("-s", "--mean_by"), 
+	help="choose one or multiple attributes you want to average by"),
+
+make_option(c("-o", "--output_suffix"), default="out.tsv", 
+	help="Output file name. [default=%default]"),
+
+make_option(c("-f", "--func"), default="mean",
+	help="choose the function <mean>, <sd>, <sum>, <median>, <entropy> [default=%default]"),
+
+make_option(c("-n", "--not_na"), type="double", default=1, 
+	help="fraction of not NA values in the vector for the mean [default=%default]")
+
 )
 
 parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
@@ -64,19 +80,18 @@ print(opt)
 
 # read options
 m <- read.table(opt$input_matrix, h=T)
-mdata <- read.table(opt$metadata, h=T, row.names=NULL, sep="\t")
-mdata$labExpId <- sapply(mdata$labExpId, function(x) gsub(",", ".", x))
+
+# Remove non-numeric columns
 char_cols <- which(sapply(m, class) == 'character')
 sprintf("WARNING: column %s is character, so it is removed from the analysis", char_cols)
-
 if (length(char_cols) == 0) {genes = rownames(m)}
 if (length(char_cols) != 0) {genes = m[,char_cols]; m = m[,-(char_cols)]}
 
 # apply the log if required
-if (opt$replace_na) {m<-replace(m, is.na(m), 0)}
-if (opt$log) { m = log10(replace(m, is.na(m), 0) + opt$pseudocount) }
+if (opt$replace_na) {m <- replace(m, is.na(m), 0)}
+if (opt$log) {m = log10(m + opt$pseudocount)}
 
-mdata = subset(mdata, labExpId %in% colnames(m))
+
 
 # ----------------
 # Functions
@@ -110,20 +125,34 @@ if (opt$func == "median") {func = my_median}
 if (opt$func == "entropy") {func = my_entropy}
 
 
+
 # apply the function to the whole matrix if no value is provided
 if (is.null(opt$mean_by)) {
-new_m = apply(m, 1, func); colnames(new_m) <- c(opt$func)
+	new_m = setNames(data.frame(apply(m, 1, func)), opt$func)
 } else {
-# apply the function to the levels of the specified factors
-mean_by = strsplit(opt$mean_by, ",")[[1]]
-meltm = melt(as.matrix(m), varnames = c("gene_index", "labExpId"))
-df = merge(meltm, unique(mdata[c("labExpId",mean_by)]), by = "labExpId")
-df$value[abs(df$value)==Inf] <- NA
-aggr = aggregate(as.formula(sprintf("value~gene_index+%s", paste(mean_by,collapse="+"))), df, func, na.action="na.pass")
-aggr = dcast(aggr, as.formula(sprintf("gene_index~%s", paste(mean_by,collapse="+"))))
-if (length(char_cols)==0) {new_m = aggr; colnames(new_m)[1] = "gene"}
-if (length(char_cols)!=0) {new_m = merge(genes, aggr, by.y="gene_index", by.x="row.names")[,-1]}
+
+	# apply the function to the levels of the specified factors
+	mean_by = strsplit(opt$mean_by, ",")[[1]]
+	df = melt(as.matrix(m), varnames = c("gene_index", "labExpId"))
+	
+	# read metadata and merge with data.frame if needed
+	if (!is.null(opt$metadata)) {
+		mdata <- read.table(opt$metadata, h=T, row.names=NULL, sep="\t")
+		mdata$labExpId <- sapply(mdata$labExpId, function(x) gsub(",", ".", x))
+		mdata = subset(mdata, labExpId %in% colnames(m))
+		df = merge(df, unique(mdata[c("labExpId", mean_by)]), by = "labExpId")
+	}
+	
+	df$value[abs(df$value)==Inf] <- NA
+	aggr = aggregate(as.formula(sprintf("value~gene_index+%s", paste(mean_by,collapse="+"))), df, func, na.action="na.pass")
+	aggr = dcast(aggr, as.formula(sprintf("gene_index~%s", paste(mean_by,collapse="+"))))
+	new_m = aggr
 }
+
+
+if (length(char_cols)==0) {new_m = cbind(gene=genes, new_m)}
+if (length(char_cols)!=0) {new_m = merge(genes, new_m, by.y="gene_index", by.x="row.names")[,-1]}
+
 
 
 
@@ -131,7 +160,9 @@ if (length(char_cols)!=0) {new_m = merge(genes, aggr, by.y="gene_index", by.x="r
 # print output
 #--------------
 
-output = sprintf('%s.by_%s.n_%s.%s', opt$func, opt$mean_by, opt$not_na, opt$output_suffix)
-write.table(new_m, sprintf('%s.tsv',output), quote=F, sep='\t', row.names=F)
-#pdf(sprintf("%s.pdf", output)); gp1; gp2; dev.off()
+
+#output = sprintf('%s.by_%s.n_%s.%s', opt$func, opt$mean_by, opt$not_na, opt$output_suffix)
+#write.table(new_m, sprintf('%s.tsv',output), quote=F, sep='\t', row.names=F)
+write.table(new_m, opt$output, quote=F, sep='\t', row.names=F)
+
 q(save='no')
