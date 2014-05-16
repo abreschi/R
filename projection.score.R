f3d4f593216441bcec068fb65a623c56acb802a7
Alessandra Breschi <alessandra.breschi@crg.es>
2014-05-16 19:24:36 +0200

    more flexible filtering

--- projection.score.R
+++ projection.score.R
@@ -16,6 +16,9 @@ make_option(c("-i", "--input"), default="stdin",
 make_option(c("-o", "--output"), default="proj.score",
 	help="Output file name WITHOUT extension [default=%default]"),
 
+make_option(c("-s", "--statistics"), 
+	help="Two-column file with header, with the row names of the input and an associated statistics by which to filter"),
+
 make_option(c("-l", "--log10"), action="store_true", default=FALSE,
 	help="Apply the log10 to the whole matrix as pre-processing step [default=%default]"),	
 
@@ -23,7 +26,7 @@ make_option(c("-p", "--pseudocount"), default=0.001,
 	help="Pseudocount to add when applying the log [default=%default]"),
 
 make_option(c("-t", "--thresholds"), default="0.1,0.2,0.25,0.3,0.35,0.4,0.5,0.6,0.7,0.8",
-	help="A comma-separated list of different variance thresholds (percentages of the max variance) [default=%default]"),
+	help="A comma-separated list of different thresholds (percentages of the max value) [default=%default]"),
 
 make_option(c("-B", "--iterations"), default=10,
 	help="Number of row permutations [default=%default]"),
@@ -35,7 +38,13 @@ make_option(c("-v", "--verbose"), action="store_true", default=FALSE,
 	help="if you want more output [default=%default]")
 )
 
-parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
+parser <- OptionParser(
+	usage = "%prog [options] file", 
+	option_list=option_list,
+	description = "Compute the projection score on a matrix given a vector of values to use as thresholds. 
+	NOTE: The matrix is used as it comes with no normalization"
+)
+
 arguments <- parse_args(parser, positional_arguments = TRUE)
 opt <- arguments$options
 if (opt$verbose) {print(opt)}
@@ -53,6 +62,7 @@ var_thresholds = strsplit(opt$thresholds, ",")[[1]]
 B = opt$iterations
 S = opt$nb_components
 
+
 if (opt$input == "stdin") {
 	m = read.table(file("stdin"), h=T)
 } else {
@@ -63,11 +73,15 @@ if (opt$log10) {
 	m = log10(m + opt$pseudocount)
 }
 
+# Read the statistics
+stats = read.table(opt$statistics, h=T)
+# Order them to be in the same order as the input matrix row names
+stats = stats[match(stats[,1], rownames(m)),]
 
 # Compute the variance for each row
-variance = apply(m, 1, var)
+#variance = apply(m, 1, var)
 # Normalize the variance by the maximum variance
-variancen = variance/max(variance)
+variancen = stats[,2]/max(stats[,2], na.rm=T)
 
 # Function to compute the alpha_2 measure
 alpha_2 = function(lambda, S) {
@@ -84,11 +98,11 @@ for (var_t in var_thresholds) {
 	m_t = m[which(variancen>var_t),]
 
 	# Compute the pca on this submatrix
-	pca1 = prcomp(t(m_t), scale.=TRUE)
+	pca1 = prcomp(t(m_t), center=FALSE, scale.=FALSE)
 
 	# Obtain a matrix of lambdas (sdev) for each permutation
 	# Rows are the components and columns are the iterations
-	lambda = replicate(B, prcomp(apply(m_t, 1, sample), scale.=TRUE)$sdev) 
+	lambda = replicate(B, prcomp(apply(m_t, 1, sample), center=FALSE, scale.=FALSE)$sdev) 
 	# Count how many times the stdev for each component in the permutation is lower than the observed one
 	lambda_counts = rowSums(apply(lambda, 2, function(x) pca1$sdev>=x))
 	
