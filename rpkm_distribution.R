17855d37254c0a0994275e33f0a1b6b09f1d77a5
Alessandra Breschi <alessandra.breschi@crg.es>
2014-06-10 17:39:52 +0200

    fill by as factor

--- rpkm_distribution.R
+++ rpkm_distribution.R
@@ -12,7 +12,6 @@ suppressPackageStartupMessages(library(plyr))
 cat("DONE\n\n")
 
 options(stringsAsFactors=F)
-pseudocount = 1e-04
 cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#000000", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") 
 
 ##################
@@ -21,26 +20,58 @@ cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#000000", "#F0E442", "#0072B2"
 
 
 option_list <- list(
-make_option(c("-i", "--input_matrix"), help="the matrix you want to analyze"),
-make_option(c("-l", "--log"), action="store_true", default=FALSE, help="apply the log [default=%default]"),
-make_option(c("-p", "--pseudocount"), type="double", help=sprintf("specify a pseudocount for the log [default=%s]",pseudocount), default=pseudocount),
-make_option(c("-m", "--metadata"), help="tsv file with metadata on matrix experiment. If empty, column names are used as labels."),
-make_option(c("-r", "--representation"), help="choose the representation <boxplot>, <density>, <histogram>, <boxviol> [default=%default]", default="boxplot"),
-make_option(c("-o", "--output"), help="additional tags for otuput", default='out'),
+
+make_option(c("-i", "--input_matrix"), 
+	help="the matrix you want to analyze"),
+
+make_option(c("-l", "--log"), action="store_true", default=FALSE, 
+	help="apply the log10 [default=%default]"),
+
+make_option(c("-p", "--pseudocount"), type="double", default = 1e-04,
+	help="specify a pseudocount for the log [default=%default]"),
+
+make_option(c("-m", "--metadata"), 
+	help="tsv file with metadata on matrix experiment. If empty, column names are used as labels."),
+
+make_option(c("-r", "--representation"), default = "boxplot",
+	help="choose the representation <boxplot>, <density>, <histogram>, <boxviol> [default=%default]"),
+
+make_option(c("-o", "--output"), default="out",
+	help="additional tags for otuput [default=%default]"),
+
+make_option(c("-d", "--outdir"), default="./",
+	help="specificy a directory for the output [default=%default]"),
+
 make_option(c("-f", "--fill_by"), help="choose the color you want to fill by. Leave empty for no fill", type='character'),
 make_option(c("-a", "--alpha_by"), help="choose the fator for the transparency in boxplot. Leave empty for no transparency", type="character"),
-make_option(c("-v", "--value"), help="give a name for the value you are plotting the distribution of [default=%default]", default="rpkm"), 
+make_option(c("-v", "--x_title"), help="give a title to the x axis [default=%default]", default="rpkm"), 
 make_option(c("-w", "--wrap"), action="store_true", help="say if you want the density plot in facet_wrap [default=%default]", default=FALSE), 
-make_option(c("-T", "--title"), help="give a title to the plot [default=%default]", default=""), 
-make_option(c("-P", "--palette"), help="palette name [default=%default]", default="cbbPalette"),
-make_option(c("-G", "--no_guide"), help="use this to remove the legend from the plot [default=%default]", default=FALSE, action="store_true"),
-make_option(c("-t", "--tags"), help="comma-separated field names you want to display in the labels. Leave default for using column names [default=%default]", default="labExpId")
+make_option(c("-T", "--title"), default="", 
+	help="give a title to the plot [default=%default]"), 
+
+make_option(c("-P", "--palette"), default="/users/rg/abreschi/R/palettes/cbbPalette.8.txt",
+	help="palette file name [default=%default]"),
+
+make_option(c("-H", "--height"), default=10, 
+	help="height of the plot in inches [default=%default]"),
+
+make_option(c("-W", "--width"), default=10, 
+	help="width of the plot in inches [default=%default]"),
+
+make_option(c("-G", "--no_guide"), default=FALSE, action="store_true", 
+	help="use this to remove the legend from the plot [default=%default]"),
+
+make_option(c("--verbose"), default=FALSE, action="store_true", 
+	help="use this for verbose mode [default=%default]"),
+
+make_option(c("-t", "--tags"), default="labExpId", 
+	help="comma-separated field names you want to display in the labels. Leave default for using column names [default=%default]")
 )
 
 parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
 arguments <- parse_args(parser, positional_arguments = TRUE)
 opt <- arguments$options
-print(opt)
+if (opt$verbose) {print(opt)}
 
 ##--------------------##
 ## CLUSTERING SAMPLES ##
@@ -48,109 +79,128 @@ print(opt)
 
 # read the matrix from the command line
 m = read.table(opt$input_matrix, h=T)
-ylab = opt$value
+ylab = opt$x_title
+palette = read.table(opt$palette, h=F, comment.char="%")$V1
 
 # remove potential gene id columns
 char_cols <- which(sapply(m, class) == 'character')
-sprintf("WARNING: column %s is character, so it is removed from the analysis", char_cols)
+if (opt$verbose) {sprintf("WARNING: column %s is character, so it is removed from the analysis", char_cols)}
 if (length(char_cols) == 0) {genes = rownames(m)}
 if (length(char_cols) != 0) {genes = m[,char_cols]; m = m[,-(char_cols)]}
 
 # substitute the matrix with its log if required by the user
 if (opt$log) {
-m = log10(replace(m, is.na(m), 0) + opt$pseudocount);
-ylab = sprintf('log10(%s+%s)', opt$value, opt$pseudocount)}
+	m = log10(replace(m, is.na(m), 0) + opt$pseudocount);
+#	ylab = sprintf('log10(%s+%s)', opt$x_axis, opt$pseudocount)
+}
 
 # prepare data.frame for ggplot
 tags = strsplit(opt$tags, ",")[[1]]
-df = melt(m, variable.name = "labExpId", value.name=opt$value)
+df = melt(as.matrix(m), varnames = c("element","labExpId"), value.name="rpkm")
+
+if(opt$verbose) {
+	print(head(df))
+}
 
 # read the metadata from the metadata file if present
 if (!is.null(opt$metadata)) {
 
-mdata = read.table(opt$metadata, h=T, sep='\t')
-mdata$labExpId <- sapply(mdata$labExpId, function(x) gsub(",", ".", x))
+	mdata = read.table(opt$metadata, h=T, sep='\t')
+	mdata$labExpId <- sapply(mdata$labExpId, function(x) gsub(",", ".", x))
+	
+	# prepare data.frame for ggplot
+	df = merge(unique(mdata[c("labExpId", tags, opt$fill_by, opt$alpha_by)]), df, by="labExpId")
+	
+	# change color palette in case length is too much
+	if (length(unique(df[,opt$fill_by])) > length(cbbPalette)) {cbbPalette <- rainbow(length(unique(df[,opt$fill_by])))}
 
-# prepare data.frame for ggplot
-df = merge(unique(mdata[c("labExpId", tags, opt$fill_by, opt$alpha_by)]), df, by="labExpId")
-
-# change color palette in case lenght is too much
-if (length(unique(df[,opt$fill_by])) > length(cbbPalette)) {cbbPalette <- rainbow(length(unique(df[,opt$fill_by])))}
+	# change the fill_by column to factor
+	if (!is.null(opt$fill_by)) {
+		df[,opt$fill_by] = as.factor(df[,opt$fill_by])
+	}
 }
 
 
 df$labels = apply(df[tags], 1, paste, collapse="_")
- 
+
+if (opt$verbose) {
+	print(head(df))
+}
+
 
 ###############
 # OUTPUT 
 ###############
 
-output = sprintf("%s.log_%s.psd_%s.%s", opt$representation, 
+output = sprintf("%s/%s.log_%s.psd_%s.%s", opt$outdir, opt$representation, 
 ifelse(opt$log, "T", "F"), ifelse(opt$log,opt$pseudocount,"NA"),opt$output)
 
 # plotting...
 
-theme_set(theme_bw())
+theme_set(theme_bw(base_size=14))
+
+width = 7; height = 7;
+
+medians = aggregate(rpkm~labels, df, median)
+lev = medians[order(medians$rpkm),]$labels
+df$labels = factor(df$labels, levels=lev)
+
+
+width = opt$width
+height = opt$height
+breaks = floor(min(df[is.finite(df[,"rpkm"]),"rpkm"])):ceiling(max(df[is.finite(df[,"rpkm"]),"rpkm"]))
+
 
 if (opt$representation == "boxplot") {
-pdf(sprintf("%s.pdf", output), h=log2(ncol(m)), w=10)
-gp = ggplot(df, aes_string(x="labels", y=opt$value))
-gp = gp + geom_boxplot(outlier.size=0.5, size=0.5, aes_string(fill=opt$fill_by, alpha=opt$alpha_by))
-gp = gp + labs(y=ylab, x='', title=opt$title)
-gp = gp + coord_flip()
-if(opt$palette == "cbbPalette"){gp = gp + scale_fill_manual(values = cbbPalette)} else {
-gp = gp + scale_fill_brewer(palette=opt$palette)}
-gp = gp + scale_alpha_manual(values=c(.5,1))
-gp = gp + scale_y_continuous(breaks = floor(min(df[is.finite(df[,opt$value]),opt$value])):ceiling(max(df[is.finite(df[,opt$value]),opt$value])))
-gp = gp + theme(axis.text=element_text(size=40/log10(length(df$labels))))
+
+	gp = ggplot(df, aes_string(x="labels", y="rpkm"))
+	gp = gp + geom_boxplot(outlier.size=0.5, size=0.5, aes_string(fill=opt$fill_by, alpha=opt$alpha_by))
+	gp = gp + labs(y=ylab, x='', title=opt$title)
+	gp = gp + coord_flip()
+	gp = gp + scale_fill_manual(values = palette)
+	gp = gp + scale_alpha_manual(values=c(.5,1))
+	gp = gp + scale_y_continuous(breaks = breaks)
 }
 
 if (opt$representation == "boxviol") {
-pdf(sprintf("%s.pdf", output), h=log2(ncol(m)), w=10)
-gp = ggplot(df, aes_string(x="labels", y=opt$value))
+gp = ggplot(df, aes_string(x="labels", y="rpkm"))
 gp = gp + geom_violin(size=.4,aes_string(fill=opt$fill_by))
 gp = gp + geom_boxplot(alpha=0.2, size=.2)
 gp = gp + labs(y=ylab, x='', title=opt$title)
 gp = gp + coord_flip()
-gp = gp + scale_fill_manual(values = cbbPalette)
+gp = gp + scale_fill_manual(values = palette)
 gp = gp + stat_summary(fun.y="mean", colour="red", geom="point", size = 3)
-gp = gp + scale_y_continuous(breaks = floor(min(df[is.finite(df[,opt$value]),opt$value])):ceiling(max(df[is.finite(df[,opt$value]),opt$value])))
-gp = gp + theme(axis.text=element_text(size=40/log10(length(df$labels))))
+gp = gp + scale_y_continuous(breaks = breaks)
 }
 
 if (opt$representation == "density") {
-h=5; w=5
-gp = ggplot(df, aes_string(x=opt$value))
+gp = ggplot(df, aes_string(x="rpkm"))
 if (opt$wrap) {
-h=7; w=7
 gp = gp + geom_density(aes_string(color=if(is.na(opt$fill_by)){NULL}else{opt$fill_by}))
 gp = gp + facet_wrap(~labels)} else {
 gp = gp + geom_density( aes_string(group="labels", color=if(is.na(opt$fill_by)){NULL}else{opt$fill_by}) )}
 gp = gp + labs(x=ylab, title=opt$title)
-gp = gp + scale_color_manual(values = cbbPalette)
-gp = gp + scale_x_continuous(breaks = floor(min(df[is.finite(df[,opt$value]),opt$value])):ceiling(max(df[is.finite(df[,opt$value]),opt$value])))
-gp = gp + theme(axis.text=element_text(size=40/log10(length(df$labels))))
-pdf(sprintf("%s.pdf", output), h=h, w=w)
+gp = gp + scale_color_manual(values = palette)
+gp = gp + scale_x_continuous(breaks = breaks)
 }
 
 if (opt$representation == "histogram") {
-pdf(sprintf("%s.pdf", output))
 df$labels = apply(df[tags], 1, paste, collapse="\n") # only in this case collapse with new line
-gp = ggplot(df, aes_string(x=opt$value))
+gp = ggplot(df, aes_string(x="rpkm"))
 gp = gp + geom_histogram(aes_string(fill=if(is.na(opt$fill_by)){NULL}else{opt$fill_by}, y='..count..'), right=TRUE, 
-origin=min(df[is.finite(df[,opt$value]),opt$value]))
+origin=min(df[is.finite(df[,"rpkm"]),"rpkm"]))
 gp = gp + facet_wrap(~labels)
 gp = gp + labs(x=ylab, y='', title=opt$title)
-gp = gp + scale_fill_manual(values = cbbPalette)
-gp = gp + scale_x_continuous(breaks = floor(min(df[is.finite(df[,opt$value]),opt$value])):ceiling(max(df[is.finite(df[,opt$value]),opt$value])))
-gp = gp + theme(axis.text=element_text(size=40/log10(length(df$labels))))
+gp = gp + scale_fill_manual(values = palette)
+gp = gp + scale_x_continuous(breaks = breaks)
 }
+
+gp = gp + theme(axis.text=element_text(size=40/log10(length(df$labels))))
+
 
 if (opt$no_guide) {gp = gp + theme(legend.position="none")}
 
-gp
 
-dev.off()
+pdf(sprintf("%s.pdf", output), w=width, h=height); gp; dev.off()
 
 q(save='no')
