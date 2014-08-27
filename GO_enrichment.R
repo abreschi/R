112ac16cc89645f987d2ae55289178e980dcb492
Alessandra Breschi <alessandra.breschi@crg.es>
2014-08-27 18:07:27 +0200

    round result output

--- GO_enrichment.R
+++ GO_enrichment.R
@@ -15,20 +15,19 @@ suppressPackageStartupMessages(library("optparse"))
 
 option_list <- list(
 make_option(c("-u", "--universe"), help="a list of human gene identifiers (ensEMBL ids), NO header"),
-make_option(c("-G", "--genes"), help="a list of human gene identifiers for the foreground (ensEMBL ids), WITH header"),
-make_option(c("-c", "--categ"), help="choose the GO category <BP>, <MF>, <CC> [default=%default]", default="BP"),
-make_option(c("-s", "--species"), help="choose the species <human>, <mouse> [default=%default]", default="human"),
-#make_option(c("-l", "--log"), action="store_true", default=FALSE, help="apply the log [default=FALSE]"),
-#make_option(c("-p", "--pseudocount"), type="double", help=sprintf("specify a pseudocount for the log [default=%s]",pseudocount), default=pseudocount),
-#make_option(c("-m", "--metadata"), help="tsv file with metadata on matrix experiment"),
-make_option(c("-o", "--output"), help="additional tags for otuput [default=%default]", default="out")
-#make_option(c("-f", "--fill_by"), help="choose the color you want to fill by [default=NA]", type='character', default=NA)
+make_option(c("-G", "--genes"), default="stdin",
+	help="a list of human gene identifiers for the foreground (ensEMBL ids), WITHOUT header [default=%default]"),
+make_option(c("-c", "--categ"), help="choose the GO category < BP | MF | CC > [default=%default]", default="BP"),
+make_option(c("-s", "--species"), help="choose the species < human | mouse | dmel > [default=%default]", default="human"),
+make_option(c("-o", "--output"), help="additional tags for otuput [default=%default]", default="out"),
+make_option(c("-d", "--output_dir"), default="./", help="directory for the output [default=%default]"),
+make_option(c("-v", "--verbose"), default=FALSE, action="store_true", help="verbose")
 )
 
 parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
 arguments <- parse_args(parser, positional_arguments = TRUE)
 opt <- arguments$options
-print(opt)
+if (opt$verbose) {print(opt)}
 
 
 
@@ -36,27 +35,35 @@ print(opt)
 ## LIBRARIES
 ##------------ 
 
-cat("Loading libraries... ")
+if (opt$verbose) {cat("Loading libraries... ")}
 
 
 suppressPackageStartupMessages(library("GO.db"))
 if (opt$species == "human") {suppressPackageStartupMessages(library("org.Hs.eg.db"))}
 if (opt$species == "mouse") {suppressPackageStartupMessages(library("org.Mm.eg.db"))}
+if (opt$species == "dmel") {suppressPackageStartupMessages(library("org.Dm.eg.db"))}
 suppressPackageStartupMessages(library("GOstats"))
 suppressPackageStartupMessages(library("plyr"))
 
-cat("DONE\n\n")
+if (opt$verbose) {cat("DONE\n\n")}
 
 ############################
 # BEGIN
 ############################
 
 U = read.table(opt$universe, h=F, col.names='hs')
-G = read.table(opt$genes, h=T, col.names='hs')
+#if (opt$genes == "stdin") {
+#	G = read.table(file("stdin"), h=T, col.names='hs')	
+#} else {
+#	G = read.table(opt$genes, h=T, col.names='hs')
+#}
 
+if (opt$genes == "stdin") {
+	G = read.table(file("stdin"), h=F, col.names='hs')	
+} else {
+	G = read.table(opt$genes, h=F, col.names='hs')
+}
 
-#orth = read.table('~/Documents/db/human-mouse/orthologs/merged_orthologs_HumanMouse_1_1_genes.tsv', h=F, col.names=c('hs','mm'))
-#genes = read.table('~/Documents/human-mouse/tissues/maxdiff_tissuespecific.tsv', h=F, col.names=c('hs','mm','sample'))
 
 # I want to create a list of parameters to perform GO enrichment on different gene sets
 
@@ -67,8 +74,11 @@ universe = unlist(mget(U$hs, org.Hs.egENSEMBL2EG, ifnotfound=NA))}
 if (opt$species == "mouse") {
 universe = unlist(mget(U$hs, org.Mm.egENSEMBL2EG, ifnotfound=NA))}
 
+if (opt$species == "dmel") {
+universe = unlist(mget(U$hs, org.Dm.egENSEMBL2EG, ifnotfound=NA))}
 
-sprintf("%s background genes; %s with a corresponding entrez id", nrow(U), length(unique(universe)))
+
+if (opt$verbose) {sprintf("%s background genes; %s with a corresponding entrez id", nrow(U), length(unique(universe)))}
 # how many genes am I able to map?
 # First thing notice that also ensembl gene ids longer than 15 characters are included
 # if I remove these genes I end up with:
@@ -77,11 +87,17 @@ sprintf("%s background genes; %s with a corresponding entrez id", nrow(U), lengt
 
 createParams = function(x, species="human") {
 	if (species == "human") {
-	ann = "org.Hs.eg.db"
-	geneset = unlist(mget(x, org.Hs.egENSEMBL2EG, ifnotfound=NA))}
+		ann = "org.Hs.eg.db"
+		geneset = unlist(mget(x, org.Hs.egENSEMBL2EG, ifnotfound=NA))
+	}
 	if (species == "mouse") {
-	ann = "org.Mm.eg.db"
-	geneset = unlist(mget(x, org.Mm.egENSEMBL2EG, ifnotfound=NA))}
+		ann = "org.Mm.eg.db"
+		geneset = unlist(mget(x, org.Mm.egENSEMBL2EG, ifnotfound=NA))
+	}
+	if (species == "dmel") {
+		ann = "org.Dm.eg.db"
+		geneset = unlist(mget(x, org.Dm.egENSEMBL2EG, ifnotfound=NA))
+	}
 	sprintf("%s foreground genes; %s with a corresponding entrez id", length(x), length(unique(geneset)))
 	pv = 1-(1-0.05)**(1/length(x))
 	params = new("GOHyperGParams",
@@ -95,8 +111,17 @@ createParams = function(x, species="human") {
 	return(params)}
 
 res = hyperGTest(createParams(G$hs, opt$species))
-write.table(summary(res), file=sprintf("%s.%s.tsv", opt$output, opt$categ), quote=F, sep="\t", row.names=F)
-htmlReport(res, file=sprintf("%s.%s.html", opt$output, opt$categ))
+
+# Reformat the output table
+df = summary(res)
+df$Pvalue = format(df$Pvalue, digits=1) 
+df$OddsRatio <- round(df$OddsRatio, 2)
+df$ExpCount <- round(df$ExpCount, 2)
+
+# Print output
+output = sprintf("%s/%s.%s", opt$output_dir, opt$output, opt$categ)
+write.table(df, file=sprintf("%s.tsv", output), quote=F, sep="\t", row.names=F)
+htmlReport(res, file=sprintf("%s.html", output))
 
 q(save='no')
 
