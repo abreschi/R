727d37d83f23b847525d079d0bab57d820df7dcf
Alessandra Breschi <alessandra.breschi@crg.es>
2015-03-20 20:33:45 +0100

    now can bin based on breaks

--- add_quantile.R
+++ add_quantile.R
@@ -1,8 +1,6 @@
 #!/usr/bin/env Rscript
 
-
 options(stringsAsFactors=FALSE)
-cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#000000", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") 
 
 ##################
 # OPTION PARSING
@@ -21,6 +19,9 @@ make_option(c("-o", "--output"), default="quantile.out.tsv",
 make_option(c("--header"), action="store_true", default=FALSE,
 	help="Use this if the input has a header [default=%default]"),
 
+make_option(c("-m", "--method"), default="quantiles",
+	help="Choose the way to bin the data: < quantiles | breaks > [default=%default]"),
+
 make_option(c("-c", "--column"), default=1,
 	help="The column with the values to divide in quantiles [default=%default]"),
 
@@ -29,6 +30,9 @@ make_option(c("-q", "--quantiles"), default=4, type="integer",
 
 make_option(c("-s", "--resolve_breaks"), default=FALSE, action="store_true",
 	help="When breaks are not unique, don't crash but create fewer quantiles [default=%default]"),
+
+make_option(c("-b", "--breaks"), default="-Inf,1,2,3,Inf", 
+	help="Breaks for the intervals, comma-separated [default=%default]"), 
 
 make_option(c("-v", "--verbose"), action="store_true", default=FALSE,
 	help="if you want more output [default=%default]")
@@ -51,6 +55,30 @@ suppressPackageStartupMessages(library(ggplot2))
 if (opt$verbose) {cat("DONE\n\n")}
 
 
+# ============== #
+# Functions      #
+# ============== #
+
+formatInterval = function(interval) {
+
+	interval = as.character(interval)
+	if (length(grep("-Inf", interval) != 0)) {
+		outInterval = sprintf("<= %s", gsub("\\)", "", strsplit(interval, split=",")[[1]][2]))
+		return(outInterval)
+	}
+	if (length(grep("Inf", interval) != 0)) {
+		outInterval = sprintf("> %s", gsub("\\(", "", strsplit(interval, split=",")[[1]][1]))
+		return(outInterval)
+	}
+
+#	left = gsub("[\\(\\]]", "", strsplit(interval, split=",")[[1]][1], perl=TRUE)
+#	right = gsub("\\)", "", strsplit(interval, split=",")[[1]][2])
+#	outInterval = sprintf("%s <= x < %s", left, right)
+	outInterval = interval
+	return(outInterval)
+}
+
+
 # ======== #
 # BEGIN    #
 # ======== #
@@ -67,6 +95,8 @@ if (opt$input == "stdin") {
 
 # Quantiles
 
+if (opt$method == "quantiles") {
+
 quantile_header = sprintf("quantile_%s", colnames(m)[opt$column])
 quant_index_header = sprintf("quant_index_%s", colnames(m)[opt$column])
 
@@ -77,6 +107,24 @@ if (opt$resolve_breaks) {
 } else {
 	m[,quantile_header] = cut_number(m[,opt$column], opt$quantiles)
 	m[,quant_index_header] = as.numeric(cut_number(m[,opt$column], opt$quantile))
+}
+
+}
+
+# Intervals
+
+if (opt$method == "breaks") {
+
+interval_header = sprintf("interval_%s", colnames(m)[opt$column])
+interval_index_header = sprintf("interval_index_%s", colnames(m)[opt$column])
+
+breaks = as.numeric(strsplit(opt$breaks, split=",")[[1]])
+m[,interval_header] = cut(m[,opt$column], breaks=breaks, include.lowest=TRUE, right=TRUE)
+m[,interval_index_header] = cut(m[,opt$column], breaks=breaks, include.lowest=TRUE, right=TRUE, labels=FALSE)
+
+# Format the interval
+m[,interval_header] = sapply(m[,interval_header], formatInterval)
+
 }
 
 
