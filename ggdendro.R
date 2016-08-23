ca4899c0c3b6b75588cefc9020f6e3fce988e3b2
Alessandra Breschi <alessandra.breschi@crg.es>
2016-08-23 13:44:36 +0200

    Now supports vertical orientation. Can add dendrogram labels (only vertical orientation)

--- ggdendro.R
+++ ggdendro.R
@@ -130,9 +130,14 @@ load_obj <- function(f)
 
 base_size = opt$base_size
 theme_set(theme_grey(base_size))
-theme_update(axis.ticks=element_blank())
-theme_update(axis.ticks.margin = unit(0.01, "inch"))
-theme_update(axis.ticks.length = unit(0.01, "inch"))
+theme_update(
+	axis.ticks=element_blank(),
+	axis.ticks.margin = unit(0.01, "inch"),
+	axis.ticks.length = unit(0.01, "inch"),
+	panel.grid.minor = element_blank(),
+	panel.grid.major = element_blank()
+)
+theme_update(panel.background = element_blank())
 
 
 
@@ -156,7 +161,10 @@ if (!is.null(opt$debug)) {m = m[sample(nrow(m), opt$debug),]}
 
 # Read palette
 if (!is.null(opt$colSide_palette)) {
-colSide_palette = as.character(read.table(opt$colSide_palette, h=F)$V1)}
+	colSide_palette_files = strsplit(opt$colSide_palette, ",")[[1]]
+	colSide_palette = sapply(colSide_palette_files, function(x)  as.character(read.table(x, h=F, sep="\t", comment.char="%")$V1), simplify=FALSE)
+	#if (opt$verbose) {cat("ColSide Palette:", colSide_palette[1], "\n")}
+}
 
 # remove potential gene id columns
 char_cols <- which(sapply(m, class) == 'character')
@@ -173,7 +181,7 @@ if (!is.null(opt$log)) {m <- log(replace(m, is.na(m), 0) + opt$pseudocount, base
 # read metadata
 if (opt$verbose) {cat(sprintf("%s: ", Sys.time()), "Read metadata... ")}
 if (!is.null(opt$col_metadata)) {
-	col_mdata = read.table(opt$col_metadata, h=T, sep="\t")
+	col_mdata = read.table(opt$col_metadata, h=T, sep="\t", quote="\"", comment.char="")
 	# read which fields are needed from the metadata
 	if (!is.null(opt$colSide_by)) {colSide_by = strsplit(opt$colSide_by, ",")[[1]]} else {colSide_by = NULL}
 	if (!is.null(opt$col_labels) && opt$col_labels != "none") {col_label_fields = strsplit(opt$col_labels,",")[[1]]} else {col_label_fields=NULL}
@@ -209,20 +217,49 @@ if (opt$dist == "p" || opt$dist =="s") {
 if (opt$verbose) {cat("DONE\n")}
 
 colHC = hclust(colDist, method=opt$hclust)
+
 colHC_data = dendro_data(as.dendrogram(colHC))
+
 col_ggdendro = ggplot(segment(colHC_data))
 col_ggdendro = col_ggdendro + geom_segment(aes(x=x, y=y, xend=xend, yend=yend))
 #col_ggdendro = col_ggdendro + geom_text(data=label(colHC_data), 
 #	aes(x=x, y=y, label=label), angle=dendro_angle, hjust=dendro_hjust) 
-if (opt$vertical) {
-	col_ggdendro = col_ggdendro + coord_flip()
-} 
-col_ggdendro = col_ggdendro + scale_x_continuous(expand=c(0.0, 0.5), labels=NULL) 
-col_ggdendro = col_ggdendro + scale_y_continuous(expand=c(0.0, 0.0), labels=NULL)
-col_ggdendro = col_ggdendro + theme(plot.margin=unit(c(0.10, 0.00, 0.00, 0.01), "inch")) # top, right, bottom, left
+col_ggdendro = col_ggdendro + theme(plot.margin=unit(c(0.00, 0.00, 0.00, 0.00), "inch")) # top, right, bottom, left
 col_ggdendro = col_ggdendro + theme_dendro()
 col_ggdendro = col_ggdendro + labs(x=NULL, y=NULL)
+col_ggdendro = col_ggdendro + scale_x_continuous(expand=c(0.0, 0.5), labels=NULL) 
+col_ggdendro = col_ggdendro + scale_y_continuous(expand=c(0.0, 0.0), labels=NULL)
+if (opt$vertical) {
+	col_ggdendro = col_ggdendro + coord_flip()
+	col_ggdendro = col_ggdendro + scale_y_reverse(expand=c(0.0, 0.0), labels=NULL)
+} 
 #col_ggdendro
+
+
+
+# ---------- Column labels ----------
+
+
+labelData = label(colHC_data)
+if (!is.null(opt$col_labels) && opt$col_labels != "none") {
+	#labelData$label <- df[,col_label_fields][match(df[,"Var2"], label(colHC_data)[,"label"])]
+	labelData$label <- df[,col_label_fields][match(label(colHC_data)[,"label"], df[,"Var2"])]
+}
+
+col_labels_inches = 0
+if (opt$col_labels != "none") {
+	gp_labels = ggplot(data=labelData)
+	gp_labels = gp_labels + geom_text(aes(x, 0, label=label, hjust=0))
+	gp_labels = gp_labels + labs(x=NULL, y=NULL)
+	gp_labels = gp_labels + theme(plot.margin=unit(c(0.00, 0.00, 0.00, 0.00), "inch")) # top, right, bottom, left
+	gp_labels = gp_labels + scale_x_continuous(expand=c(0.0, 0.5), labels=NULL) 
+	gp_labels = gp_labels + scale_y_continuous(expand=c(0.0, 0.0), limits=c(0,1), labels=NULL)
+	if (opt$vertical) {
+		gp_labels = gp_labels + coord_flip()
+	}
+	col_labels_inches = max(strwidth(labelData$label, units="in", cex=base_size*(as.numeric(theme_get()$axis.text$size))*par()$cex/par()$ps))
+}
+
 
 
 # -------------------- Column Side Colors ------------
@@ -233,6 +270,14 @@ col_limits = label(colHC_data)[,3]
 #if (opt$verbose) {print(col_limits)}
 
 if (!is.null(opt$colSide_by)) {
+	if (!is.null(opt$colSide_palette)) {
+		if (length(colSide_palette) == 1) {
+			colSide_palette = rep(colSide_palette, length(colSide_by))}
+		if (length(colSide_palette) >1 && length(colSide_palette) != length(colSide_by)) {
+			cat("ERROR: Inconsistent number of column factors and palettes\n"); 
+			q(save='no')
+		}
+    }
 	i=1;
 	for (colSide in colSide_by) {
 		colSide_data = unique(df[c("Var2", colSide)])
@@ -241,15 +286,25 @@ if (!is.null(opt$colSide_by)) {
 		ColSide = ColSide + scale_x_discrete(limits = col_limits, labels=NULL, expand=c(0,0))
 		ColSide = ColSide + scale_y_discrete(labels=NULL, expand=c(0,0))
 		if (!is.null(opt$colSide_palette)) {
-			ColSide = ColSide + scale_fill_manual(values=colSide_palette)
+			ColSide = ColSide + scale_fill_manual(values=colSide_palette[[i]])
+		} else {
+			ColSide = ColSide + scale_fill_hue()
 		}
-		ColSide = ColSide + theme(plot.margin=unit(c(0.00, 0.00, 0.00, 0.01),"inch"))
+#
+#		if (!is.null(opt$colSide_palette)) {
+#			ColSide = ColSide + scale_fill_manual(values=colSide_palette)
+#		}
+		ColSide = ColSide + theme(plot.margin=unit(c(0.00, 0.00, 0.00, 0.00),"inch"))
 		ColSide = ColSide + labs(x=NULL, y=NULL)
+		if (opt$vertical) {
+			ColSide = ColSide + coord_flip()
+		} 
 		ColSide_legends[[i]] = g_legend(ColSide)
 		ColSide = ColSide + theme(legend.position="none")
 		ColSides[[i]] = ColSide; i=i+1;
 	}
 }
+
 
 
 #ggsave('test.pdf', plot=ColSides[[1]], h=10, w=7)
@@ -279,8 +334,8 @@ TOT_W = opt$width
 
 left_margin = 0.1
 right_margin = 0.1
-vert_inter_space = 0.2
-colbar_h = 0.30
+vert_inter_space = 0.1
+colbar_h = 0.50          # height of each column bar
 
 # >>> Dendro viewport (default) parameters <<<<<
 
@@ -288,6 +343,19 @@ dendro_y = colbar_h*length(ColSides) # + col_labels_inches
 dendro_h = TOT_H - 0.05
 dendro_w = TOT_W - (right_margin + left_margin)
 
+if (opt$vertical) {
+	dendro_y = 0.05
+	dendro_h = TOT_H - (right_margin + left_margin)
+	dendro_w = TOT_W - 0.05
+	dendro_w = dendro_w - col_labels_inches
+	dendro_w = dendro_w - (colbar_h+0.02)*length(ColSides)
+}
+
+if (!is.null(opt$colSide_by)) {
+	legend_width_inch = max(sapply(ColSide_legends, function(x) sum(sapply(x$widths, convertUnit, "in"))))
+	legend_height_inch = sum(sapply(ColSide_legends, function(x) sum(sapply(x$heights, convertUnit, "in")))) + 0.1*(length(ColSide_legends)-1)
+	dendro_w = dendro_w - (legend_width_inch + vert_inter_space)
+}
 
 if (opt$verbose) {cat(sprintf("%s: ", Sys.time()), "Creating viewports... \n")}
 if (opt$verbose) {cat('col legend vp.. \n')}
@@ -295,9 +363,10 @@ if (opt$verbose) {cat('col legend vp.. \n')}
 # >>>>> Column side legends viewport <<<<<<<<<<<<<<<<<
 
 if (!is.null(opt$colSide_by)) {
-	legend_width_inch = max(sapply(ColSide_legends, function(x) sum(sapply(x$widths, convertUnit, "in"))))
-	legend_height_inch = sum(sapply(ColSide_legends, function(x) sum(sapply(x$heights, convertUnit, "in")))) + 0.1*(length(ColSide_legends)-1)
-	dendro_w = TOT_W - (legend_width_inch + right_margin + left_margin + vert_inter_space) 
+#	legend_width_inch = max(sapply(ColSide_legends, function(x) sum(sapply(x$widths, convertUnit, "in"))))
+#	legend_height_inch = sum(sapply(ColSide_legends, function(x) sum(sapply(x$heights, convertUnit, "in")))) + 0.1*(length(ColSide_legends)-1)
+	#dendro_w = TOT_W - (legend_width_inch + right_margin + left_margin + vert_inter_space) 
+#	dendro_w = dendro_w - (legend_width_inch + vert_inter_space) 
 	side_legend_vps = list()
 	legend_h = 0
 	for (i in 1:length(colSide_by)) {
@@ -309,6 +378,16 @@ if (!is.null(opt$colSide_by)) {
 			default.units = "inch",
 			just = c("left", "top")
 		)
+		if (opt$vertical) {
+			side_legend_vp = viewport(
+				y = (TOT_H - 0.01) - legend_h,
+				x = left_margin + dendro_w + vert_inter_space + col_labels_inches + (colbar_h+0.02)*length(ColSides),
+				h = sum(sapply(ColSide_legends[[i]]$heights, convertUnit, "in")),
+				w = legend_width_inch,
+				default.units = "inch",
+				just = c("left", "top")
+			)
+		}
 		legend_h = sum(sapply(ColSide_legends[[i]]$heights, convertUnit, "in"))
 		side_legend_vps[[i]] = side_legend_vp
 	}
@@ -329,6 +408,16 @@ if (!is.null(opt$colSide_by)){
 			default.units = "inch",
 			just = c("left", "bottom") 
 		)
+		if (opt$vertical) {
+			ColSide_vps[[i]] = viewport(
+				y = 0.05,
+				x = dendro_w + colbar_h*(i-1),
+				h = dendro_h,
+				w = colbar_h,
+				default.units = "inch",
+				just = c("left", "bottom") 
+			)
+		}
 	#	ColSide_label_vps[[i]] = viewport(
 	#		y = 0.01 + colbar_h*(i-1),
 	#		x = left_margin + dendro_w,
@@ -338,16 +427,40 @@ if (!is.null(opt$colSide_by)){
 	#		just = c("left", "bottom")
 	#	)
 	}
-	dendro_h = TOT_H - (colbar_h+0.02)*length(ColSides)
+#	if (opt$vertical) {
+#		dendro_w = dendro_w - (colbar_h+0.02)*length(ColSides)
+#	}
+	if (!opt$vertical) {
+		dendro_h = TOT_H - (colbar_h+0.02)*length(ColSides)
+	}
 } 
 
 
 
 if (opt$verbose) {cat('dendro vp...\n')}
 
+
+# >>>>> Column labels viewport <<<<<<<<<<<<<<<<<
+
+if (opt$col_labels != "none") {
+	if (opt$vertical) {
+		col_labels_vp = viewport(
+			y = 0.05,
+			x = dendro_w + vert_inter_space + (colbar_h+0.02)*length(ColSides),
+			h = dendro_h,
+			w = col_labels_inches,
+			default.units = "inch",
+			just = c("left", "bottom") 
+		)
+	}
+} 
+
+if (opt$verbose) {cat('col side vp... \n')}
+
+
 # >>> Column dendrogram viewport <<<<<<<<<
 
-dendro_vp_y = colbar_h*length(ColSides) # + col_labels_inches
+dendro_vp_y = dendro_y # + col_labels_inches
 dendro_vp_x = 0.1
 
 colDendro_vp = viewport(
@@ -375,20 +488,13 @@ pdf(opt$output, h = TOT_H, w=TOT_W)
 if (!is.null(opt$colSide_by)) {
 	for (i in 1:length(ColSide_vps)) {
 		print(ColSides[[i]], vp=ColSide_vps[[i]], newpage=FALSE)
+#		print(gp_labels, vp=ColSide_vps[[i]], newpage=FALSE)
 	#	grid.text(colSide_by[i], x=unit(1,"npc"), just="left", vp=ColSide_label_vps[[i]], gp=gpar(face="bold"))
 	}
 }
 
 if (opt$verbose) {cat('print col side colors\n')}
 
-## Print row side colors
-#if (!is.null(opt$rowSide_by)) {
-#	for (i in 1:length(RowSide_vps)) {
-#		print(RowSides[[i]], vp=RowSide_vps[[i]], newpage=FALSE)
-#		grid.text(rowSide_by[i], y=unit(1,"npc"), just="right", rot=90, vp=RowSide_label_vps[[i]], gp=gpar(face="bold"))
-#	}
-#}
-#
 ## Print column dendrogram
 print(col_ggdendro, vp=colDendro_vp, newpage=FALSE)
 
@@ -399,6 +505,11 @@ if (!is.null(opt$colSide_by)) {
 		all_side_legends = ColSide_legends
 		pushViewport(side_legend_vps[[i]]); grid.draw(all_side_legends[[i]]); upViewport()
 	}
+}
+
+# Print dendrogram labels
+if (opt$col_labels != "none") {
+	print(gp_labels, vp=col_labels_vp, newpage=FALSE)
 }
 
 
